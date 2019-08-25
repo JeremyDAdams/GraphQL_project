527e3cbbe3f861095469e17f974d2d15cc6447d9
Jeremy Adams <Beoron@gmail.com>
2019-08-24 19:35:50 -0500

    Add Apollo server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-console.log("Testing project");
+const app = express();
 
-console.log(process.env.secret);
+app.use(cors());
+
+const schema = gql`
+    type Query {
+        me: User
+    }
+
+    type User {
+        username: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        me: () => {
+            return {
+                username: 'Jeremy Adams',
+            };
+        },
+    },
+};
+
+// const data = {
+//     me: {
+//         username: 'Jeremy Adams',
+//     },
+// };
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+    console.log('Apollo Server on http://localhost:8000/graphql');
+});
